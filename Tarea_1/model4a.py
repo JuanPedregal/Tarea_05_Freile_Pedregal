5513eb13b6f5019cb3bd08a80a502e19ecd8f509
Mateo Freile <mfreile@udesa.edu.ar>
2021-07-25 23:11:43 -0300

    Completando comentarios

--- Tarea_1/model4a.py
+++ Tarea_1/model4a.py
@@ -4,29 +4,32 @@ Name : model4a
 Group : 
 With QGIS : 31608
 """
-
+# Parte del material obtenido de https://github.com/sebastianhohmann/gis_course/tree/master/QGIS/research_course
+# Resumen: 
+# Se importan los paquetes necesarios para estos procesos
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
 from qgis.core import QgsProcessingParameterFeatureSink
 import processing
 
-
+# Se crea e inicia el modelo 4a
 class Model4a(QgsProcessingAlgorithm):
-
+    # Se obtendrán 3 layers
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterFeatureSink('Fixgeo_wlds', 'fixgeo_wlds', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Fixgeo_countries', 'fixgeo_countries', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Intersection', 'intersection', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
-
+    # Se ejecutaran 4 algoritmos
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
         feedback = QgsProcessingMultiStepFeedback(4, model_feedback)
         results = {}
         outputs = {}
-
-        # Corregir geometrías wlds
+        #################################################
+        ### Se corrigen las geometrias de "clean.shp" ###
+        #################################################
         alg_params = {
             'INPUT': 'C:/Udesa/Herramientas/Clase_5/output/clean.shp',
             'OUTPUT': parameters['Fixgeo_wlds']
@@ -38,7 +41,9 @@ class Model4a(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Corregir geometrías countries
+        ####################################################################
+        ### Se corrigen las geometrias de "ne_10m_admin_0_countries.shp" ###
+        ####################################################################
         alg_params = {
             'INPUT': 'C:/Udesa/Herramientas/Clase_5/input/ne_10m_admin_0_countries.shp',
             'OUTPUT': parameters['Fixgeo_countries']
@@ -49,8 +54,9 @@ class Model4a(QgsProcessingAlgorithm):
         feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
-
-        # Intersección
+        #####################################
+        ### Se intersectan estos dos .shp ###
+        #####################################
         alg_params = {
             'INPUT': outputs['CorregirGeometrasWlds']['OUTPUT'],
             'INPUT_FIELDS': ['GID'],
@@ -65,8 +71,9 @@ class Model4a(QgsProcessingAlgorithm):
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
-
-        # Estadísticas por categorías
+        ###########################################################################################
+        ### Se calculan estadísticas por categorías de 'ADMIN' del layer obtenido anteriormente ###
+        ###########################################################################################
         alg_params = {
             'CATEGORIES_FIELD_NAME': ['ADMIN'],
             'INPUT': outputs['Interseccin']['OUTPUT'],
