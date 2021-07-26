37cc6ab4a44e9815eddc6341e6f91d9e8c858507
Mateo Freile <mfreile@udesa.edu.ar>
2021-07-25 23:24:29 -0300

    Completando comentarios

--- Tarea_1/model4c.py
+++ Tarea_1/model4c.py
@@ -4,7 +4,9 @@ Name : model4c
 Group : 
 With QGIS : 31608
 """
-
+# Parte del material obtenido de https://github.com/sebastianhohmann/gis_course/tree/master/QGIS/research_course
+# Resumen: Calcular area de los paises
+# Se importan los paquetes necesarios para estos procesos
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
@@ -12,23 +14,24 @@ from qgis.core import QgsProcessingParameterFeatureSink
 from qgis.core import QgsCoordinateReferenceSystem
 import processing
 
-
+# Se crea y da inicio al modelo 4c
 class Model4c(QgsProcessingAlgorithm):
-
+    # Se obtendrán 4 layers
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterFeatureSink('Areas_out', 'areas_out', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Countries_fixgeo', 'countries_fixgeo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Countries_reprojected', 'countries_reprojected', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Countries_drop_fields', 'countries_drop_fields', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-
+     # Se ejecutaran 5 algoritmos
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
         feedback = QgsProcessingMultiStepFeedback(5, model_feedback)
         results = {}
         outputs = {}
-
-        # Quitar campo(s) - countries_drop_fields
+        #########################################################################
+        ### Se quitan columnas innecesarias de "ne_10m_admin_0_countries.shp" ###
+        #########################################################################
         alg_params = {
             'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','APCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','GDP_MD_EST','POP_YEAR','LASTCENSUS','GDP_YEAR','ECONOMY','INCOME_GRP','WIKIPEDIA','FIPS_10_','ISO_A2','ISO_A3_EH','ISO_N3','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_A3_IS','ADM0_A3_US','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FR','NAME_EL','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_VI','NAME_ZH','MAPCOLOR9','ADMIN_2','ISO_A3_2'],
             'INPUT': 'C:/Udesa/Herramientas/Clase_5/input/ne_10m_admin_0_countries.shp',
@@ -41,7 +44,9 @@ class Model4c(QgsProcessingAlgorithm):
         if feedback.isCanceled():
             return {}
 
-        # Reproyectar capa - countries_reprojected
+        ########################################################
+        ### Se reproyecta el anterior layer en  'ESRI:54034' ###
+        ########################################################
         alg_params = {
             'INPUT': outputs['QuitarCamposCountries_drop_fields']['OUTPUT'],
             'OPERATION': '',
@@ -54,8 +59,9 @@ class Model4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
-
-        # Corregir geometrías
+        #####################################################
+        ### Se corrigen las geometrias del anterior layer ###
+        #####################################################
         alg_params = {
             'INPUT': outputs['ReproyectarCapaCountries_reprojected']['OUTPUT'],
             'OUTPUT': parameters['Countries_fixgeo']
@@ -66,8 +72,9 @@ class Model4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
             return {}
-
-        # Calculadora de campos
+        #############################################
+        ### Se calcula el area para cada poligono ###
+        #############################################
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'km2area',
@@ -83,8 +90,9 @@ class Model4c(QgsProcessingAlgorithm):
         feedback.setCurrentStep(4)
         if feedback.isCanceled():
             return {}
-
-        # Save vector features to file
+        ##################################################
+        ### Se guarda el layer anterior en formato csv ###
+        ##################################################
         alg_params = {
             'DATASOURCE_OPTIONS': '',
             'INPUT': outputs['CalculadoraDeCampos']['OUTPUT'],
